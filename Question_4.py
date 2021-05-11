2738dc69a17ced389c29bc57335a9402c8e4012b
albertodallora <albertodallora01@gmail.com>
2021-05-11 12:21:43 +0200

    Nelder-Mead Opt

--- Question_4.py
+++ Question_4.py
@@ -41,42 +41,74 @@ def SM_opt(init_set):
         # Sharpness Metric
         threshold = 0.2
         gain = 1
-        step = 0.02
+        step = 0.5
         min_obj = 0
         SM = 0
         C_reg = 1
         C_pos = 1
         C_norm = 1
-        opt_set = init_set
+        obj = np.zeros(shape=(len(dm)+1,1))
 
         # Optimization process
-        while (gain) > threshold:
-            DM_set = opt_set + np.random.uniform(-step, step, size=len(dm))  # Random walk
-            dm.setActuators(DM_set)
-            img = grabframes(5, Camera_Index)
+        # Compute Nelder-Mead Vertices
+        for i in range(len(dm)):
+            for k in range(len(dm) + 1):
+                temp = np.zeros(len(dm))
+                if i > 0:
+                    temp[i] = step
+                vertex_matrix[k,i] = init_set + temp
+        # Compute SM for all the vertices
+        for k in range(len(dm) + 1):
+            dm.setActuators(vector_matrix[k,])
+            img = grabframes(3, Camera_Index)
             x_intens = 0
             y_intens = 0
             # Compute centroid
             intens_sum = np.matrix.sum(img[-1, i, k])
             for i in range(1024):
-                for k in range(1280):
-                    x_intens += k * img[-1, i, k]
-                    y_intens += i * img[-1, i, k]
+                for m in range(1280):
+                    x_intens += m * img[-1, i, m]
+                    y_intens += m * img[-1, i, m]
             centroid_x = x_intens / intens_sum
             centroid_y = y_intens / intens_sum
             positioning = np.sqrt(centroid_x ** 2 + centroid_y ** 2)
             # Compute second-order metric
             for i in range(1024):
-                for k in range(1280):
-                    SM += img[-1, i, k] * ((k**2 + i**2))
+                for n in range(1280):
+                    SM += img[-1, i, k] * ((n-640)**2 + (i-512)**2)
 
-            obj = C_norm * SM + C_reg * np.linalg.norm(DM_set) + C_pos * positioning
-            # If the result has improved, store it
-            if obj < min_obj:
-                gain = obj - min_obj
-                min_obj = obj
-                opt_SM = SM
-                opt_set = DM_set
+            obj[k] = C_norm * SM + C_reg * np.linalg.norm(DM_set) + C_pos * positioning
+        # Opt. Iterations
+        while gain > threshold:
+            # Change the worst vertex
+            worst_index = np.argmax(obj)
+            vector_matrix[worst_index,] = vector_matrix[worst_index,] - np.ones((len(dm),1)*0.25
+            #Compute SM for the new vertex
+            dm.setActuators(vector_matrix[worst_index,])
+            img = grabframes(3, Camera_Index)
+            x_intens = 0
+            y_intens = 0
+            # Compute centroid
+            intens_sum = np.matrix.sum(img[-1, i, k])
+            for i in range(1024):
+                for m in range(1280):
+                    x_intens += m * img[-1, i, m]
+                    y_intens += m * img[-1, i, m]
+            centroid_x = x_intens / intens_sum
+            centroid_y = y_intens / intens_sum
+            positioning = np.sqrt(centroid_x ** 2 + centroid_y ** 2)
+            # Compute second-order metric
+            for i in range(1024):
+                for n in range(1280):
+                    SM += img[-1, i, k] * (((n - 640) ** 2 + (i - 512) ** 2))
+
+            worst_obj = obj[worst_index]
+            obj[worst_index] = C_norm * SM + C_reg * np.linalg.norm(DM_set) + C_pos * positioning
+
+            gain = worst_obj - obj[worst_index]
+            if gain < threshold:
+                opt_SM = SM     #the last computed
+                opt_set = vertex_matrix[worst_index,]
 
         return opt_set, opt_SM
 
@@ -86,20 +118,11 @@ if __name__ == "__main__":
     from dm.thorlabs.dm import ThorlabsDM
 
     with ThorlabsDM() as dm:
-        #Sharpness Optimization
-        opt_set, max_sharp = sharpness_opt(np.zeros(shape=(43, 1)))     #Static aberrations
-        #opt_set, max_sharp = sharpness_opt(np.random.uniform(-1, 1, size=len(dm)))     #Random aberration
-        #opt_set, max_sharp = sharpness_opt(np.zeros(shape=(43,1))[40:43]=-1)     #Defocus aberration
+        #SM Optimization
+        opt_set, opt_SM = SM_opt(np.zeros(shape=(43, 1)))     #Static aberrations
+        #opt_set, opt_SM = SM_opt(np.random.uniform(-1, 1, size=len(dm)))     #Random aberration
+        #opt_set, opt_SM = SM_opt(np.zeros(shape=(43,1))[40:43]=-1)     #Defocus aberration
 
-        # Half-Width Optimization
-        #opt_set, min_r = half_width_opt(np.zeros(shape=(43, 1)))  # Static aberrations
-        #opt_set, min_r = half_width_opt(np.random.uniform(-1, 1, size=len(dm)))     #Random aberration
-        #opt_set, min_r = half_width.opt(np.zeros(shape=(43,1))[40:43]=-1)     #Defocus aberration
-
-        # Sharpness-Edge Optimization
-        #opt_set, max_sharp_edge = sharpness_edge_opt(np.zeros(shape=(43, 1)))  # Static aberrations
-        #opt_set, max_sharp_edge = sharpness_edge_opt(np.random.uniform(-1, 1, size=len(dm)))     #Random aberration
-        #opt_set, max_sharp_edge = sharpness_edge_opt(np.zeros(shape=(43,1))[40:43]=-1)     #Defocus aberration
 
         # Plotting
         dm.setActuators(opt_set)
